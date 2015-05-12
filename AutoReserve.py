bcef62da0bdb32af56628d0d25982617d984a7c3
Jason Yao <jy1299@nyu.edu>
2015-05-12 15:19:02 -0400

    boilerplate in place

--- AutoReserve.py
+++ AutoReserve.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 __author__ = 'Jason Yao'
+
+# Majour imports
+import sys
+
+# Package dependent imports
+import settings
+
+# Modularizes our code via a main method
+def main():
+  print 'Hello there', sys
+  # Command line args are in sys.argv[1], sys.argv[2] ..
+  # sys.argv[0] is the script name itself and can be ignored
+
+# Standard boilerplate to call the main() function to begin the program.
+if __name__ == '__main__':
+  main()
